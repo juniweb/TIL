e751d413a4de1d6e7df3137a69a12d8230ab7548
juniweb <juniwebp@gmail.com>
2020-08-04 08:54:12 +0900

    스토어 속성 모듈화

--- Vue/vue-todo/src/store/store.js
+++ Vue/vue-todo/src/store/store.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import * as getters from "./getters";
+import * as mutations from "./mutations";
+
 Vue.use(Vuex);
 
 const storage = {
@@ -22,33 +25,6 @@ export const store = new Vuex.Store({
     state: {
         todoItems: storage.fetch()
     },
-    getters: {
-        storedTodoItems(state) {
-            return state.todoItems;
-        }
-    },
-    mutations: {
-        addOneItem(state, todoItem) {
-            console.log('addOneItem', todoItem);
-            const obj = { completed: false, item: todoItem };
-            localStorage.setItem(todoItem, JSON.stringify(obj));
-            state.todoItems.push(obj);
-        },
-        removeOneItem(state, payload) {
-            console.log('removeOneItem', payload);
-            localStorage.removeItem(payload.todoItem.item);
-            state.todoItems.splice(payload.index, 1);
-        },
-        toggleOneItem(state, payload) {
-            console.log('toggleOneItem', payload);
-            state.todoItems[payload.index].completed = !state.todoItems[payload.index].completed;
-            localStorage.removeItem(payload.todoItem.item);
-            localStorage.setItem(payload.todoItem.item, JSON.stringify(payload.todoItem));
-        },
-        clearAllItems(state) {
-            console.log('clearAllItems');
-            state.todoItems = [];
-            localStorage.clear();
-        }        
-    }
+    getters,
+    mutations
 });
