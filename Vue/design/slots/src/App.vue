6a5d540460ff3ee9a72847d07dad86904fd078d9
juniweb <juniwebp@gmail.com>
2020-08-22 13:00:15 +0900

    컴포넌트 디자인 패턴- Slot 완료

---
+++ Vue/design/slots/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <ul>
+      <item>
+        아이템1
+      </Item>
+      <item>
+        아이템2
+        <button>click me</button>
+      </Item>
+      <item>
+        <div style="display: flex;">
+          아이템3
+          <img src="./assets/github.png" alt="gitbub" style="width:100px; height:50px;">
+        </div>
+      </Item>
+      <item>
+        <div style="color:blue; font-size:20px;">
+          아이템4
+        </div>
+      </Item>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Item from './components/Item.vue'
+
+export default {
+  components: {
+    Item
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ Vue/design/slots/src/components/Item.vue
@@ -0,0 +1,13 @@
+<template>
+  <li>
+      <slot>
+          <!-- NOTE: 등록하는 곳에서 정의할 화면 영역 -->
+      </slot>
+  </li>
+</template>
+
+<script>
+export default {
+
+}
+</script>
