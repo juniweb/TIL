f236c35e28edc1766b62f167c09bf90cc45f3d42
juniweb <juniwebp@gmail.com>
2020-08-22 14:36:38 +0900

    컴포넌트 디자인 패턴- Renderless Component 완료

---
+++ Vue/design/renderless/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <fetch-data url="https://jsonplaceholder.typicode.com/users/1">
+      <div slot-scope="{ response, loading }">
+        <div v-if="!loading">
+          <p>name : {{ response.name }}</p>
+          <p>email : {{ response.email }}</p>
+        </div>
+        <div v-if="loading">
+          <h1>Loading...</h1>
+        </div>        
+      </div>
+    </fetch-data>
+  </div>
+</template>
+
+<script>
+import FetchData from './components/FetchData.vue'
+
+export default {
+  components: {
+    FetchData
+  }
+}
+</script>
+
+<style>
+</style>
