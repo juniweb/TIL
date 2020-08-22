07a316abddb203c5a281a56d645b7f73ccff43d6
juniweb <juniwebp@gmail.com>
2020-08-22 13:54:54 +0900

    컴포넌트 디자인 패턴- Controlled Component 완료

---
+++ Vue/design/controlled/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <check-box v-model="checked"></check-box>
+  </div>
+</template>
+
+<script>
+import CheckBox from './components/CheckBox.vue'
+
+export default {
+  components: {
+    CheckBox
+  },
+  data() {
+    return {
+      checked: false
+    }
+  }
+}
+</script>
+
+<style>
+</style>
