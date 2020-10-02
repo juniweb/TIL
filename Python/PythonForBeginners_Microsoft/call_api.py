9b38e109f14b4482f14682522a694428f858de84
juniweb <juniwebp@gmail.com>
2020-10-02 13:44:02 +0900

    파일 Rename

---
+++ Python/PythonForBeginners_Microsoft/call_api.py
@@ -0,0 +1,27 @@
+
+# Use the requests library to simplify making a REST API call from Python
+import requests
+
+# We will need the json library to read the data passed back
+# by the web service
+import json
+
+# Sample REST API address
+users_address = "https://reqres.in/api/users?page=2"
+
+address = users_address + "?page=2"
+
+headers = {
+    'content-type': 'application/json'
+}
+
+parameters = {}
+
+response = requests.get(address, headers=headers, params=parameters)
+
+response.raise_for_status()
+
+results = response.json()
+
+print(json.dumps(results))
+
--- Python/PythonForBeginners_Microsoft/helpers.py
+++ Python/PythonForBeginners_Microsoft/helpers.py
@@ -1,4 +1,9 @@
+# from colorama import init, Fore
+
 def display(message, is_waring=False):
     if is_waring:
-        print('Waring!!')
-    print(message)
+        # print(Fore.RED + message)
+        print(message)
+    else:
+        # print(Fore.BLUE + message)
+        print(message)
